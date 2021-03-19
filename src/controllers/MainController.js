531254a197958db5ca6a402fbb609f5f4b7ef583
Sander Jochems <sanderjochems@hotmail.nl>
2021-03-19 19:16:07 +0100

    Update MainController.js

--- src/controllers/MainController.js
+++ src/controllers/MainController.js
@@ -1,4 +1,11 @@
-import {Controller, GridController, MainView} from "../CROWDR";
+import {
+	Controller,
+	GridController,
+	MainView,
+	PlaceableController,
+	SettingsController,
+	TerrainController
+} from "../CROWDR";
 
 export default class MainController extends Controller {
 	constructor() {
@@ -6,5 +13,8 @@ export default class MainController extends Controller {
 		this.mainView = new MainView();
 
 		this.gridController = new GridController();
+		this.placeableController = new PlaceableController();
+		this.settingsController = new SettingsController();
+		this.terrainController = new TerrainController();
 	}
 }
